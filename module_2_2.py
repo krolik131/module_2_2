d1d53c95e48cb4a4b94c848b6def207c7a84293d
krolik131 <kostya-leteha@mail.ru>
2024-09-11 12:04:11 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+
+print('Введите первое целое число')
+first = int(input())
+print('Введите второе целое число')
+second = int(input())
+print('Введите третье целое число')
+third = int(input())
+if first != second and first != third and second != third:
+    print(0)
+else: print(2)
+
